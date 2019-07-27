0e4a68785fa499ae491440d7c1018784824ed74b
nastioushka <nastia@nastia.nastia>
2019-07-27 21:02:08 +0200

    Initial commit

---
+++ js/goUp.js
@@ -0,0 +1,28 @@
+'use strict'
+
+var goUp        = null;
+var body        = null;
+var html        = null;
+var scrolleG     = 0;
+var scrolleS     = 0;
+
+goUp = document.getElementById("goUp");
+body = document.body;
+html = document.documentElement;
+
+//Fonctions actions en scrolle
+function onScroll()
+{
+  if(html.scrollTop > scrolleG || body.scrollTop > scrolleS)
+  {
+    goUp.style.display = "none";
+  }
+  else
+  {
+    goUp.style.display = "block";
+  };
+  scrolleG = html.scrollTop;
+  scrolleS = body.scrollTop;
+};
+
+window.onscroll = function() {onScroll()};
